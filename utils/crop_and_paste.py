bd5951b41de8f887de01ae7141dd50959431e070
Anton O. Burygin <a.burygin@norsi-trans.ru>
2021-05-11 22:41:05 +0300

    refactor code + add ploting result

--- utils/crop_and_paste.py
+++ utils/crop_and_paste.py
@@ -3,6 +3,7 @@ import matplotlib as mpl
 import numpy as np
 import cv2
 
+
 def _overlay_image_alpha(img, img_overlay, x, y, alpha_mask):
     """Overlay `img_overlay` onto `img` at (x, y) and blend using `alpha_mask`.
 
@@ -29,25 +30,35 @@ def _overlay_image_alpha(img, img_overlay, x, y, alpha_mask):
     img_crop[:] = alpha * img_overlay_crop + alpha_inv * img_crop
 
 
+def plot_image(img, figsize):
+    # print crop_and_pasted images
+    dpi = mpl.rcParams['figure.dpi']
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_axes([0, 0, 1, 1])
+
+    ax.axis('off')
+    ax.imshow(img)
+    plt.show()
+
+
 def crop_and_paste(donor_image_path, donor_mask_path, target_image_path):
     # https://stackoverflow.com/questions/62813546/how-do-i-crop-an-image-based-on-custom-mask-in-python
 
-    # crop mask wiht defect
-    temple = plt.imread(donor_image_path)
+    # crop mask with defect
+    donor_image = plt.imread(donor_image_path)
     target_image = plt.imread(target_image_path)
-    heart = plt.imread(donor_mask_path, cv2.IMREAD_GRAYSCALE)
-    _, mask = cv2.threshold(heart, thresh=180, maxval=255, type=cv2.THRESH_BINARY)
+    donor_mask = plt.imread(donor_mask_path, cv2.IMREAD_GRAYSCALE)
+    _, mask = cv2.threshold(donor_mask, thresh=180, maxval=255, type=cv2.THRESH_BINARY)
 
-    mask, _, _ = cv2.split(mask)
+    donor_image_x, donor_image_y, _ = donor_image.shape
+    donor_mask_x, donor_mask_y = mask.shape
 
-    temple_x, temple_y, _ = temple.shape
-    heart_x, heart_y = mask.shape
+    x_donor_mask = min(donor_image_x, donor_mask_x)
+    x_half_donor_mask = mask.shape[0] // 2
 
-    x_heart = min(temple_x, heart_x)
-    x_half_heart = mask.shape[0] // 2
-
-    heart_mask = mask[x_half_heart - x_heart // 2: x_half_heart + x_heart // 2 + 1, :temple_y]
-    masked = cv2.bitwise_and(temple, temple, mask=heart_mask)
+    donor_mask_mask = mask[x_half_donor_mask - x_donor_mask // 2: x_half_donor_mask + x_donor_mask // 2 + 1,
+                      :donor_image_y]
+    masked = cv2.bitwise_and(donor_image, donor_image, mask=donor_mask_mask)
 
     tmp = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
     _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
@@ -63,16 +74,16 @@ def crop_and_paste(donor_image_path, donor_mask_path, target_image_path):
 
     # print crop_and_pasted images
     dpi = mpl.rcParams['figure.dpi']
-    figsize = temple.shape[1] / float(dpi), temple.shape[0] / float(dpi)
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_axes([0, 0, 1, 1])
+    figsize = donor_image.shape[1] / float(dpi), donor_image.shape[0] / float(dpi)
 
-    ax.axis('off')
-    ax.imshow(masked)
-    plt.show()
+    plot_image(donor_image, figsize)
+    plot_image(target_image, figsize)
+    plot_image(donor_mask, figsize)
+    plot_image(masked, figsize)
+    plot_image(img_result, figsize)
 
 
 if __name__ == '__main__':
-    crop_and_paste(donor_image_path='IMG_0441.jpg',
-                   donor_mask_path='IMG_0441_Perc_1.086%.jpg',
-                   target_image_path='IMG_0451.jpg')
+    crop_and_paste(donor_image_path='IMG_0355.jpg',
+                   donor_mask_path='IMG_0355_Perc_2.691%.jpg',
+                   target_image_path='IMG_0331.jpg')
